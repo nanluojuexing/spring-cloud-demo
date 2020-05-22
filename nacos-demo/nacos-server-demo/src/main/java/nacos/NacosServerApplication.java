0fd901fa6364533677560ba5ad5992e0bb17311d
yangpengwei <yangpengwei@xdf.cn>
2020-05-22 17:56:48 +0800

    增加nacos的模块

---
+++ nacos-demo/nacos-server-demo/src/main/java/nacos/NacosServerApplication.java
@@ -0,0 +1,14 @@
+package nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosServerApplication.class,args);
+    }
+}
